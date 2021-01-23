72020ff4dc24bdd557bdfa1eff07dc83c815ac21
Nethmi Kodikara <kadnkodikara@students.nsbm.lk>
2021-01-23 12:42:40 +0530

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,10 @@
+export const Data = [
+    {
+        question:'How to sign up on projects platform?',
+        answer: 'answer'
+    },
+    {
+        question:'Do i have to be signed in in order to sign up for projects?',
+        answer: 'answer'
+    }
+];
